fc515745a2c28e3a8b2efad9d4cc88933f7f05de
Ena Sakai <enamar2807@gmail.com>
2025-06-18 19:43:38 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Favorite from "../components/Favorite";
+import Image from "next/image";
+
+
+
+export default function Page() {
+	return (
+		<div>
+			<Image src='/images/S__46317596.jpg' alt='私の画像' width={800} height={500} className="w-full max-w-2xl object-contain rounded-2xl" />
+			<h1 className="text-5xl text-pink-200 font-bold">酒井映奈</h1>
+			<Favorite name="佐藤璃果ちゃん" src="/images/S__46383168_0.jpg" alt="璃果ちゃんの画像" />
+		</div>
+	)
+}
---
+++ src/components/Favorite.tsx
@@ -0,0 +1,15 @@
+type FavoriteProps = {
+    className?: string;
+    name: string;
+    src: string;
+    alt: string;
+}
+
+export default function Favorite({ className, name, src, alt }: FavoriteProps) {
+    return (
+        <div className={`${className}`}>
+           <Image src={src} alt={alt} width={800} height={500} className="w-full object-contain" />
+           <h3 className="text-xl">{name}</h3>
+        </div>
+    )
+}
